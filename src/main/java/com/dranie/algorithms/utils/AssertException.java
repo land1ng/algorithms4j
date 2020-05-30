2f2b5a303e39594f1c49f2bda5e1f01c2c150fe1
Ding Ran <dranfree@outlook.com>
2020-05-30 12:14:14 +0800

    用于算法编写的Maven依赖项和几个工具类

---
+++ src/main/java/com/dranie/algorithms/utils/AssertException.java
@@ -0,0 +1,13 @@
+package com.dranie.algorithms.utils;
+
+/**
+ * 断言异常
+ *
+ * @author dranfree
+ * @since 2019.05.05
+ */
+public class AssertException extends RuntimeException {
+    private static final long serialVersionUID = 6723388368533444910L;
+    public AssertException() { }
+    public AssertException(String message) { super(message); }
+}
---
+++ src/main/java/com/dranie/algorithms/utils/Optimizable.java
@@ -0,0 +1,16 @@
+package com.dranie.algorithms.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 可优化的类或方法
+ *
+ * @author dranfree
+ * @since 2020.05.30
+ */
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.SOURCE)
+public @interface Optimizable { }
