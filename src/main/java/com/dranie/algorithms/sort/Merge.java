0621b8559f885a718eb38331d8be720cecf0b67e
Ding Ran <dranfree@outlook.com>
2020-05-30 12:15:38 +0800

    自顶向下的归并排序及相应的优化

---
+++ src/main/java/com/dranie/algorithms/sort/Merge.java
@@ -0,0 +1,61 @@
+package com.dranie.algorithms.sort;
+
+import com.dranie.algorithms.utils.Optimizable;
+
+import java.util.Arrays;
+
+/**
+ * 自顶向下的归并排序
+ *
+ * @auxthor dranfree
+ * @since 2020.05.30
+ */
+@Optimizable
+public class Merge extends SortCommon {
+
+    public void sort(int[] a) {
+        sort(a, new int[a.length], 0, a.length - 1);
+    }
+
+    @Optimizable
+    protected void sort(int[] a, int[] aux, int lo, int hi) {
+        // 递归的终止条件：数组长度为1的时候自然就是有序的
+        if (lo >= hi - 1)
+            return;
+        int mi = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mi);       // 左半边排序
+        sort(a, aux, mi + 1, hi);   // 右半边排序
+        merge(a, aux, lo, mi, hi);  // 合并子数组
+    }
+
+    /**
+     * 借助辅助数组归并两个子数组，前提是两个子数组是有序的。
+     *
+     * @param a     原数组
+     * @param aux   辅助数组
+     * @param lo    左子数组下界
+     * @param mi    左子数组上界
+     * @param hi    右子数组上界
+     */
+    protected void merge(int[] a, int[] aux, int lo, int mi, int hi) {
+        // 1.首先将 [lo, hi] 拷贝到辅助数组中相应的位置
+        System.arraycopy(a, lo, aux, lo, hi + 1 - lo);
+        // 2.合并两个子数组
+        // 维护两个子数组的两个指针
+        int i = lo, j = mi + 1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mi)          a[k] = aux[j++];
+            else if (j > hi)          a[k] = aux[i++];
+            else if (less(aux, i, j)) a[k] = aux[i++];
+            else                      a[k] = aux[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 3, -1, 5, 6, -3, 44, -34, 33};
+        new Merge().sort(a);
+        System.out.println("sorted: " + Arrays.toString(a));
+        new Merge().testPerformance(1000000);
+        new MergeOptimized().testPerformance(1000000);
+    }
+}
---
+++ src/main/java/com/dranie/algorithms/sort/MergeOptimized.java
@@ -0,0 +1,31 @@
+package com.dranie.algorithms.sort;
+
+/**
+ * 归并排序的几个优化
+ *
+ * @author dranfree
+ * @since 2020.05.30
+ */
+public class MergeOptimized extends Merge {
+
+    private static final int INSERTION_BOUND = 15;
+
+    private static final Insertion INSERTION = new Insertion();
+
+    @Override
+    protected void sort(int[] a, int[] aux, int lo, int hi) {
+        // 递归的终止条件：数组长度为1的时候自然就是有序的
+        if (lo >= hi - 1)
+            return;
+        // 优化点1：小数组使用插入排序
+        if (lo >= hi - INSERTION_BOUND)
+            INSERTION.sort(a, lo, hi);
+        int mi = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mi);       // 左半边排序
+        sort(a, aux, mi + 1, hi);   // 右半边排序
+        // 优化点2：比较左子数组上界和右子数组下界，提前结束排序。
+        if (less(a, mi, mi + 1))
+            return;
+        merge(a, aux, lo, mi, hi);  // 合并子数组
+    }
+}
