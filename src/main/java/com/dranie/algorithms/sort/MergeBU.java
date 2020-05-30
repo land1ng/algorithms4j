829b9c2537115f19ac16928a256d6b4dc82d904c
Ding Ran <dranfree@outlook.com>
2020-05-30 12:52:06 +0800

    自底向上的归并排序的小优化

--- src/main/java/com/dranie/algorithms/sort/MergeBU.java
+++ src/main/java/com/dranie/algorithms/sort/MergeBU.java
@@ -18,9 +18,13 @@ public class MergeBU extends Merge {
     public void sort(int[] a) {
         int[] aux = new int[a.length];
         // 关键点：我们直接从最底部长度为1的子数组开始归并，避免了递归的消耗。
-        for (int sz = 1; sz < a.length; sz += sz)               // sz：子数组大小，每次循环之后都会翻倍。
-            for (int lo = 0; lo < a.length - sz; lo += sz + sz) // lo：子数组索引
+        for (int sz = 1; sz < a.length; sz += sz)                 // sz：子数组大小，每次循环之后都会翻倍。
+            for (int lo = 0; lo < a.length - sz; lo += sz + sz) { // lo：子数组索引
+                // 和自顶向下归并相同的优化：通过比较左子数组上界和右子数组下界的元素大小来决定是否需要执行归并
+                if (less(a, lo + sz -1, lo + sz))
+                    continue;
                 merge(a, aux, lo, lo + sz -1, Math.min(lo + sz + sz - 1, a.length - 1));
+            }
     }
 
     public static void main(String[] args) {
