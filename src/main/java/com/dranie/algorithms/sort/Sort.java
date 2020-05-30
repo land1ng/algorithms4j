37ac7d65a1c70be7801ac3431af9827c6e69ec35
Ding Ran <dranfree@outlook.com>
2020-05-30 12:14:46 +0800

    排序算法接口和抽象类

---
+++ src/main/java/com/dranie/algorithms/sort/Sort.java
@@ -0,0 +1,16 @@
+package com.dranie.algorithms.sort;
+
+/**
+ * 排序接口
+ *
+ * @author dranfree
+ * @since 2020.05.30
+ */
+public interface Sort {
+    /**
+     * 排序数组，数组中每个元素都不为空！
+     *
+     * @param a
+     */
+    void sort(int[] a);
+}
---
+++ src/main/java/com/dranie/algorithms/sort/SortCommon.java
@@ -0,0 +1,70 @@
+package com.dranie.algorithms.sort;
+
+import com.dranie.algorithms.utils.Stopwatch;
+
+/**
+ * 一些排序要用到的通用方法
+ *
+ * @author dranfree
+ * @since 2020.05.30
+ */
+public abstract class SortCommon implements Sort {
+
+    /**
+     * 比较两个对象
+     *
+     * @return
+     */
+    protected boolean less(int[] a, int i, int j) {
+        return a[i] < a[j];
+    }
+
+    protected void exch(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
+    /**
+     * 测试排序性能
+     *
+     * @param n 数量级
+     */
+    protected void testPerformance(int n) {
+        Stopwatch watch = new Stopwatch();
+        // 1.完全随机数组
+        int[] a1 = createRandom(n);
+        watch.start("[" + this.getClass().getSimpleName() + "] " + "随机");
+        sort(a1);
+        watch.finishReset();
+        // 2.正向有序数组
+        int[] a2 = createSorted(n);
+        watch.start("[" + this.getClass().getSimpleName() + "] " + "有序");
+        sort(a2);
+        watch.finishReset();
+        // 3.反向有序数组
+        int[] a3 = createInvert(n);
+        watch.start("[" + this.getClass().getSimpleName() + "] " + "反序");
+        sort(a3);
+        watch.finishReset();
+    }
+
+    private int[] createRandom(int n) {
+        int[] a = createSorted(n);
+        Shuffle.shuffle(a);
+        return a;
+    }
+    private int[] createSorted(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++)
+            a[i] = i + 1;
+        return a;
+    }
+    private int[] createInvert(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++)
+            a[i] = n - i;
+        return a;
+    }
+}
