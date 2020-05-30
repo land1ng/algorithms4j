fed1ea5b6a6f6429463bf40a7e4e77621c16a361
Ding Ran <dranfree@outlook.com>
2020-05-30 19:27:04 +0800

    堆排序

---
+++ src/main/java/com/dranie/algorithms/sort/HeapSort.java
@@ -0,0 +1,64 @@
+package com.dranie.algorithms.sort;
+
+import java.util.Arrays;
+
+/**
+ * 堆排序
+ *
+ * @author dranfree
+ * @since 2020.05.30
+ */
+public class HeapSort extends SortCommon {
+
+    /**
+     * 排序数组，数组中每个元素都不为空！
+     *
+     * @param a
+     */
+    public void sort(int[] a) {
+        // 1.构造堆
+        int hi = a.length - 1;
+        for (int k = hi / 2; k >= 0; k--)
+            sink(a, k, hi);
+        // 2.循环交换堆顶（最大的那个元素）和堆末尾元素，然后修复堆。
+        while (hi > 0) {
+            exch(a, 0, hi--); // 缩小堆
+            sink(a, 0, hi);   // 修复堆
+        }
+    }
+
+    /**
+     * 下沉
+     *
+     * @param k0
+     * @param h0
+     */
+    private void sink(int[] a, int k0, int h0) {
+        int k = k0 + 1;
+        int h = h0 + 1;
+        while (2 * k <= h) {
+            int i = 2 * k;
+            // 选择两个子节点中较大的那一个
+            if (i < h && less(a, i - 1, i))
+                i++;
+            // 让父节点与子节点中较大的那个交换
+            if (less(a, k - 1, i - 1))
+                exch(a, k - 1, i - 1);
+            else
+                break;
+            k = i;
+        }
+    }
+
+    public static void main(String[] args) {
+//        int[] a = createRandom(100);
+//        int[] a = createSorted(100);
+        int[] a = createInvert(100);
+        System.out.println("b: " + Arrays.toString(a));
+        new HeapSort().sort(a);
+        System.out.println("a: " + Arrays.toString(a));
+        SortCommon.testPerformance(1000000,
+                new MergeOptimized(),
+                new HeapSort());
+    }
+}
