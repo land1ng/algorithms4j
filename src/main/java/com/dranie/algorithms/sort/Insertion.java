f45059551ffe5ed1e1caae1fc5770fcf27ebbe7d
Ding Ran <dranfree@outlook.com>
2020-05-30 12:15:19 +0800

    插入排序和排序排序

---
+++ src/main/java/com/dranie/algorithms/sort/Insertion.java
@@ -0,0 +1,36 @@
+package com.dranie.algorithms.sort;
+
+/**
+ * @author dranfree
+ * @since 2020.05.30
+ */
+public class Insertion extends SortCommon {
+
+    /**
+     * 排序数组，数组中每个元素都不为空！
+     *
+     * @param a
+     */
+    public void sort(int[] a) {
+        sort(a, 0, a.length - 1);
+    }
+
+    /**
+     * 排序指定的子数组部分
+     *
+     * @param a
+     * @param lo
+     * @param hi
+     */
+    public void sort(int[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo; j--) {
+                if (less(a, j, j - 1)) {
+                    exch(a, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/dranie/algorithms/sort/Selection.java
@@ -0,0 +1,27 @@
+package com.dranie.algorithms.sort;
+
+/**
+ * 选择排序：选择右边最小的元素
+ *
+ * @author dranfree
+ * @since 2020.05.30
+ */
+public class Selection extends SortCommon {
+
+    /**
+     * 排序数组，数组中每个元素都不为空！
+     *
+     * @param a
+     */
+    public void sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int k = i; // 记录右边最小的元素的索引
+            for (int j = i + 1; j < a.length; j++) {
+                if (less(a, j, i))
+                    k = j;
+            }
+            if (k != i)
+                exch(a, i, k);
+        }
+    }
+}
