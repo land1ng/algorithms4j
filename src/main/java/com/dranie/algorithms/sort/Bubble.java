6e4e1ab2979bd0c42536478bb8221526013df62c
Ding Ran <dranfree@outlook.com>
2020-05-31 14:28:57 +0800

    原始冒泡排序

---
+++ src/main/java/com/dranie/algorithms/sort/Bubble.java
@@ -0,0 +1,34 @@
+package com.dranie.algorithms.sort;
+
+import com.dranie.algorithms.sort.benchmark.BenchmarkUtil;
+import com.dranie.algorithms.utils.Optimizable;
+
+/**
+ * 冒泡排序：从左往右遍历，相邻元素比较，较大元素往上冒。
+ *
+ * @author dranfree
+ * @since 2020.05.31
+ */
+@Optimizable
+public class Bubble extends SortAdapter {
+
+    /**
+     * 排序数组，数组中每个元素都不为空！
+     *
+     * @param a
+     */
+    @Override
+    public void sort(int[] a) {
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (less(a, j + 1, j))
+                    exch(a, j + 1, j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BenchmarkUtil.check(new Bubble());
+        BenchmarkUtil.benchmark(new Bubble());
+    }
+}
