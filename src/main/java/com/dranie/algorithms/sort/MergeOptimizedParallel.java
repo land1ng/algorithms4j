867a5717dd6b77029f07eb1f3124c5e6e33d0a5c
Ding Ran <dranfree@outlook.com>
2020-06-28 01:11:20 +0800

    + TODO 并行的归并排序

---
+++ src/main/java/com/dranie/algorithms/sort/MergeOptimizedParallel.java
@@ -0,0 +1,15 @@
+package com.dranie.algorithms.sort;
+
+/**
+ * TODO 写一个并行的归并排序
+ *
+ * @author dranfree
+ * @since 2020.06.28
+ */
+public class MergeOptimizedParallel extends MergeOptimized {
+
+    @Override
+    public void sort(int[] a) {
+        super.sort(a);
+    }
+}
