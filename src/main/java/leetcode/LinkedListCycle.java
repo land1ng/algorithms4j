fa2e7c2b4abcdf4c1577eed84d19d688d8265087
GuanShan <guanshan@fhldtech.com>
2021-04-16 17:55:59 +0800

    判断链表是否有环

---
+++ src/main/java/leetcode/LinkedListCycle.java
@@ -0,0 +1,80 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author dingdong
+ * @since 2021/4/16
+ */
+public class LinkedListCycle {
+
+    public static void main(String[] args) {
+
+        ListNode tail = new ListNode(12);
+        ListNode node2 = new ListNode(12, tail);
+        ListNode node3 = new ListNode(12, node2);
+        ListNode node4 = new ListNode(12, node3);
+        ListNode node5 = new ListNode(12, node4);
+        ListNode node6 = new ListNode(12, node5);
+        ListNode node7 = new ListNode(12, node6);
+
+        ListNode head = new ListNode(12, node7);
+
+        // create cycle
+        tail.next = node4;
+
+        System.out.println(hasCycle(head));
+        System.out.println(hasCycleUseHash(head));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        // 处理边界
+        if (head == null || head.next == null) {
+            return false;
+        }
+        // 快慢指针
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasCycleUseHash(ListNode head) {
+        // 处理边界
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Set<ListNode> walked = new HashSet<>();
+        for (ListNode current = head; current != null; current = current.next) {
+            if (walked.contains(current)) {
+                return true;
+            }
+            walked.add(current);
+        }
+        return false;
+    }
+
+
+    public static class ListNode {
+
+        final int val;
+
+        ListNode next;
+
+        ListNode(int x) {
+            this(x, null);
+        }
+
+        ListNode(int x, ListNode next) {
+            this.val = x;
+            this.next = next;
+        }
+    }
+}
