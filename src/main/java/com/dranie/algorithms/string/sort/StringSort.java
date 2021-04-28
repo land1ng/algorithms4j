499d99c26ac06a2b858f0a74d1662e9a4b07d250
GuanShan <guanshan@fhldtech.com>
2021-04-28 12:07:13 +0800

    字符串排序（待完善）

---
+++ src/main/java/com/dranie/algorithms/string/sort/StringSort.java
@@ -0,0 +1,11 @@
+package com.dranie.algorithms.string.sort;
+
+import com.dranie.algorithms.sort.base.BaseSort;
+
+/**
+ * @author dingdong
+ * @since 2021/4/27
+ */
+public interface StringSort extends BaseSort<String> {
+
+}
