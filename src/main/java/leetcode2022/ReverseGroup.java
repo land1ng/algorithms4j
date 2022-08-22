afed9d8f1f076116cb6217b50e3c0a7a16987bc1
Ran.Ding <e-ran.ding2@geely.com>
2022-08-22 19:01:30 +0800

    TODO K个一组翻转链表

---
+++ src/main/java/leetcode2022/ReverseGroup.java
@@ -0,0 +1,19 @@
+package leetcode2022;
+
+import leetcode.struct.ListNode;
+
+/**
+ * K个一组翻转链表
+ *
+ * @author Ran.Ding
+ * @since 2022/8/22
+ */
+public class ReverseGroup {
+
+    // oldHead1 -> oldTail2
+    // oldHead2 -> oldTail3
+    public static ListNode reverseGroup(ListNode head, int k) {
+        // TODO
+        return head;
+    }
+}
