7f39c8c3b304ebe47f89ff7a21a9a21124a22c12
burndy <dranfree@outlook.com>
2022-08-24 19:50:39 +0800

    LEETCODE:二叉树前/中/后序遍历

---
+++ src/main/java/leetcode2022/bst/BstIt.java
@@ -0,0 +1,57 @@
+package leetcode2022.bst;
+
+import leetcode.struct.TreeNode;
+import leetcode.struct.TreeNodeUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树遍历
+ *
+ * @author RanYeah
+ * @since 2022/8/24
+ */
+public class BstIt {
+
+    public static void main(String[] args) {
+        TreeNode root = TreeNodeUtil.build(new Integer[]{ 1, null, 2, 3 });
+        System.out.println(traversal(root));
+    }
+
+    public static List<Integer> traversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    // 中序遍历
+    private static void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }
+
+    // 前序遍历
+    private static void preorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        res.add(node.val);
+        preorder(node.left, res);
+        preorder(node.right, res);
+    }
+
+    // 后序遍历
+    private static void postorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        postorder(node.left, res);
+        postorder(node.right, res);
+        res.add(node.val);
+    }
+}
